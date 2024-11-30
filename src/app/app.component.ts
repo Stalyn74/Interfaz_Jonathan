e63b81ecf6da8d33db283d8b60f77cce39415b7c
Alex Mateo <jdv.escobar@yavirac.edu.ec>
2024-11-30 18:57:01 -0500

    Deberes actualizados de Interfaces

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'DEBERES INTERFACES DE DISEÑO';
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    if (this.router.url === '/') {
+      this.router.navigate(['/CASA']); 
+    }
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,19 +1,18 @@
-import { Routes, RouterModule } from '@angular/router';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { provideRouter, Routes } from '@angular/router';
+import { CASAComponent } from './CASA/casa.component';
 import { Deber1Component } from './Deber1/Deber1.component';
 import { Deber2Component } from './Deber2/Deber2.component';
 import { Deber3Component } from './Deber3/Deber3.component';
-import { NgModule } from '@angular/core';
-
 
 export const routes: Routes = [
-    {path:'Deber1', component:Deber1Component},
-    {path:'Deber2', component:Deber2Component},
-    {path:'Deber3', component:Deber3Component}
+  { path: 'Deber1', component: Deber1Component},
+  { path: 'Deber2', component: Deber2Component },
+  { path: 'Deber3', component: Deber3Component },
+  { path: 'CASA', component: CASAComponent },
+  { path: '', redirectTo: '/CASA', pathMatch: 'full' }, 
 ];
 
-
-@NgModule({
-    imports: [RouterModule.forRoot(routes)],
-    exports: [RouterModule]
-  })
-  export class AppRoutingModule {}
+bootstrapApplication(CASAComponent, {
+  providers: [provideRouter(routes)],
+}).catch((err) => console.error(err));
